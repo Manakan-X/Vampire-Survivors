8a1e298cf75291168e2182a66befda2433bbc688
Manakan <per0u0me@gmail.com>
2023-11-02 17:06:43 +0900

    キャラクターの移動処理　FPSの制限

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,4 +4,25 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 5.0f;
+
+    private void Start()
+    {
+        // FPSを60に制限する
+        Application.targetFrameRate = 60;
+    }
+
+    private void Update()
+    {
+        // キー入力を取得
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        // 移動ベクトルを計算
+        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0.0f);
+        moveDirection.Normalize();
+
+        // キャラクターの移動
+        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    }
 }
