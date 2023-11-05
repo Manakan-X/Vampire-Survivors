ea9e31de5ad05a7303c9f94b0e73933e47d2d977
Manakan <per0u0me@gmail.com>
2023-11-05 21:52:01 +0900

    キャラの移動処理をrigidbody2Dに変更

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,25 +4,43 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float moveSpeed = 5.0f;
+    [Header("移動速度")]
+    public float moveSpeed;
+
+    private Rigidbody2D rb;       // コンポーネントの取得用
+
+    private float horizontal;   // x軸(水平)方向の入力の値の代入用
+    private float vertical;     // y軸(垂直)方向の入力の値の代入用
 
     private void Start()
     {
-        // FPSを60に制限する
-        Application.targetFrameRate = 60;
+        // このスクリプトがアタッチされているゲームオブジェクト、にアタッチされているコンポーネントの中から
+        // <指定>したコンポーネントの情報を取得して、左辺に用意した変数に代入
+        rb = GetComponent<Rigidbody2D>();
     }
 
     private void Update()
     {
-        // キー入力を取得
-        float horizontalInput = Input.GetAxis("Horizontal");
-        float verticalInput = Input.GetAxis("Vertical");
+        // InputManagerのHorizontalに登録してあるキーが入力されたら、水平方向の入力値として代入
+        horizontal = Input.GetAxis("Horizontal");
+        // InputManagerのVerticalに登録してあるキーが入力されたら、垂直方向の入力値として代入
+        vertical = Input.GetAxis("Vertical");
+    }
 
-        // 移動ベクトルを計算
-        Vector3 moveDirection = new Vector3(horizontalInput, verticalInput, 0.0f);
-        moveDirection.Normalize();
+    private void FixedUpdate()
+    {
+        // 移動
+        Move();
+    }
 
-        // キャラクターの移動
-        transform.Translate(moveDirection * moveSpeed * Time.deltaTime);
+    /// <summary>
+    /// 移動
+    /// </summary>
+    private void Move()
+    {
+        // 斜めの移動距離を正規化処理を行い均一化する
+        Vector3 dir = new Vector3(horizontal, vertical, 0).normalized;
+        // velocity(速度)に新しい値を代入して、ゲームオブジェクトを移動させる
+        rb.velocity = dir * moveSpeed;
     }
 }
