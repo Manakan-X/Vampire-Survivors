2d6a5106b8bf412ed8655da2f8cd5382ec226832
Manakan <per0u0me@gmail.com>
2023-12-05 20:19:26 +0900

    お金を配列の中からランダムに取得する処理

--- Assets/Script/Money.cs
+++ Assets/Script/Money.cs
@@ -4,5 +4,21 @@ using UnityEngine;
 
 public class Money : MonoBehaviour
 {
+    public int[] moneyValues = { 5, 20, 50 };
 
+    int GetRandomMoney()
+    {
+        int randomIndex = Random.Range(0, moneyValues.Length);
+        return moneyValues[randomIndex];
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // お金のオブジェクトに侵入してきたオブジェクトが、コンペアタグ"Player"の場合
+        if (col.CompareTag("Player"))
+        {
+            int earnedMoney = GetRandomMoney();
+            Debug.Log("Earned Money: " + earnedMoney);
+        }
+    }
 }
